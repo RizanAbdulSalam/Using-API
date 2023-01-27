c40141cd8f11578031eebaa42a86c0b627b4b3b6
RizanAbdulSalam <82836256+RizanAbdulSalam@users.noreply.github.com>
2023-01-27 21:39:48 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,18 @@
+// https://dog.ceo/api/breeds/image/random
+// .then -> Promises!
+let dogImg = document.getElementById('randomDogImg')
+let dogBtn = document.getElementById('dogBtn')
+
+const imgDog = () => {
+    fetch('https://dog.ceo/api/breeds/image/random').then(
+        Response => Response.json()
+    ).then(
+        json => {
+            console.log(json)
+            dogImg.innerHTML = `<img src = '${json.message}'/>`
+        }
+
+    )
+}
+
+dogBtn.onclick = () => imgDog()=3
